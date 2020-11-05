ad35e795ca1b9aa7c33e4ca8880d2b3b4a3ef2d0
cebasypia <cebasypia.tskm8@gmail.com>
2020-11-05 01:12:47 +0000

    Add search & show actions

---
+++ app/controllers/api/v1/tweets_controller.rb
@@ -0,0 +1,38 @@
+class Api::V1::TweetsController < ActionController::API
+  before_action :set_client
+
+  def search
+    @tweets = []
+    since_id = nil
+    count = (params[:count] || 15).to_i
+    max_id = params[:max_id] || 0
+    # リツイートを除く、検索ワードにひっかかった最新10件のツイートを取得する
+    tweets = @client.search(
+      "#{params[:word]}#{params[:from]}",
+      lang: params[:lang],
+      tweet_mode: "extended",
+      count: 10,
+      result_type: params[:result_type],
+      exclude: "retweets",
+      since_id: since_id,
+      max_id: max_id.to_i - 1
+    )
+    tweets.take(count).each do |tweet|
+      @tweet = Tweet.new(tweet)
+      @tweets.push(@tweet)
+    end
+    render json: @tweets.sort_by { |tweet| tweet.id }.reverse
+  end
+
+  def show
+    tweet = @client.status(params[:id], tweet_mode: "extended")
+    @tweet = Tweet.new(tweet)
+    render json: @tweet
+  end
+
+  private
+
+  def set_client
+    @client = TwitterApi.client
+  end
+end
