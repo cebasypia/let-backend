8c5be035f52e81df09cb875c602f1881aa434240
cebasypia <cebasypia.tskm8@gmail.com>
2020-11-12 03:52:09 +0000

    Match to Auth0

--- db/schema.rb
+++ db/schema.rb
@@ -12,10 +12,10 @@
 
 ActiveRecord::Schema.define(version: 2020_09_24_090022) do
 
-  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.string "sub"
-    t.string "name"
-    t.string "introduction"
+  create_table "users", id: :string, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "name", default: "", null: false
+    t.string "description", default: "", null: false
+    t.string "profileImageUrl", default: "", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
