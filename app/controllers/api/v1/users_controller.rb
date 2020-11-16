0504ff0a971acc1d26a50dc03e63e526e46ab3b2
cebasypia <cebasypia.tskm8@gmail.com>
2020-11-16 07:48:30 +0000

    Add upload avatar and update user.profileImageUrl

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -18,6 +18,16 @@ class Api::V1::UsersController < ActionController::API
   end
 
   def update
+    if params[:user][:file] && params[:user][:file] != @current_user.profileImageUrl
+      blob = ActiveStorage::Blob.create_after_upload!(
+        io: StringIO.new(Base64.decode64(params[:user][:file].split(",")[1])),
+        filename: "#{@current_user.name}.png",
+        content_type: "image/png",
+      )
+      @current_user.avatar.attach(blob)
+      profile_image_url = url_for(@current_user.avatar)
+      @current_user.update(profileImageUrl: profile_image_url)
+    end
     if @current_user.update(user_params)
       render json: @current_user, status: :ok
     else
