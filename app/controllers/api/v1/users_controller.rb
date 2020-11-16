c981dbb335407b77949627722dc75c01352e8ef8
cebasypia <cebasypia.tskm8@gmail.com>
2020-11-16 12:54:19 +0000

    Add cases url or base64

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -18,15 +18,10 @@ class Api::V1::UsersController < ActionController::API
   end
 
   def update
-    if params[:user][:file] && params[:user][:file] != @current_user.profileImageUrl
-      blob = ActiveStorage::Blob.create_after_upload!(
-        io: StringIO.new(Base64.decode64(params[:user][:file].split(",")[1])),
-        filename: "#{@current_user.name}.png",
-        content_type: "image/png",
-      )
-      @current_user.avatar.attach(blob)
-      profile_image_url = url_for(@current_user.avatar)
-      @current_user.update(profileImageUrl: profile_image_url)
+    if params[:user][:file].match(/https?:\/\/[\S]+/)
+      @current_user.update(profileImageUrl: params[:user][:file])
+    elsif params[:user][:file].match(/data:image\/jpeg;base64/)
+      @current_user.update(profileImageUrl: attach_avatar(params[:user][:file]))
     end
     if @current_user.update(user_params)
       render json: @current_user, status: :ok
@@ -43,5 +38,15 @@ class Api::V1::UsersController < ActionController::API
 
   def user_params
     params.require(:user).permit(:name, :description)
+  end
+
+  def attach_avatar(file)
+    blob = ActiveStorage::Blob.create_after_upload!(
+      io: StringIO.new(Base64.decode64(file.split(",")[1])),
+      filename: "#{@current_user.name}.png",
+      content_type: "image/png",
+    )
+    @current_user.avatar.attach(blob)
+    url_for(@current_user.avatar)
   end
 end
