feb3b03cce51db3b92630a99ddcb02bc616054bd
cebasypia <cebasypia.tskm8@gmail.com>
2020-11-18 06:12:16 +0000

    Add max_id to get next tweets

--- app/controllers/api/v1/tweets_controller.rb
+++ app/controllers/api/v1/tweets_controller.rb
@@ -5,7 +5,7 @@ class Api::V1::TweetsController < ActionController::API
     @tweets = []
     since_id = nil
     count = (params[:count] || 15).to_i
-    max_id = params[:max_id] || 0
+    max_id = (params[:max_id].to_i - 1) || -1
     # リツイートを除く、検索ワードにひっかかった最新10件のツイートを取得する
     tweets = @client.search(
       "#{params[:word]}#{params[:from]}",
@@ -15,7 +15,7 @@ class Api::V1::TweetsController < ActionController::API
       result_type: params[:result_type],
       exclude: "retweets",
       since_id: since_id,
-      max_id: max_id.to_i - 1
+      max_id: max_id
     )
     tweets.take(count).each do |tweet|
       @tweet = Tweet.new(tweet)
@@ -38,10 +38,12 @@ class Api::V1::TweetsController < ActionController::API
 
   def timeline
     @tweets = []
+    max_id = params[:max_id] == '' ? get_max_id(params[:screen_name]) : (params[:max_id].to_i - 1)
     tweets = @client.user_timeline(
       params[:screen_name],
       tweet_mode: "extended",
       count: 20,
+      max_id: max_id
     )
     tweets.take(20).each do |tweet|
       @tweet = Tweet.new(tweet)
@@ -54,5 +56,10 @@ class Api::V1::TweetsController < ActionController::API
 
   def set_client
     @client = TwitterApi.client
+  end
+
+  def get_max_id(screen_name)
+    last_tweet = @client.user_timeline(screen_name, count: 1)[0]
+    last_tweet.attrs[:id_str].to_i
   end
 end
